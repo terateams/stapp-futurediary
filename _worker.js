20e261b0f462e7a3da35f66aaa0a34f306562217
Jett Wang <jamiesun.net@gmail.com>
2024-04-18 12:50:01 +0800

    更新未来日记页面配置

--- _worker.js
+++ _worker.js
@@ -2,9 +2,23 @@ export default {
     async fetch(request, env) {
         let url = new URL(request.url);
         const newHost = 'https://futurediary.streamlit.app';
-        // 确保包括完整的路径和查询字符串
         let newUrl = newHost + url.pathname + url.search;
         let new_request = new Request(newUrl, request);
-        return fetch(new_request); // 如果目标是转发请求，应使用 fetch 而非 env.ASSETS.fetch
+
+        // 发送新请求并获取响应
+        let response = await fetch(new_request);
+
+        // 检查是否为重定向响应
+        if (response.status >= 300 && response.status < 400) {
+            // 可以选择修改响应或不处理重定向
+            // 如下行可注释掉，使得重定向不被浏览器处理
+            // return response;
+            return new Response(response.body, {
+                status: 200, // 或其他非重定向状态码
+                headers: response.headers
+            });
+        }
+
+        return response;
     },
 };
