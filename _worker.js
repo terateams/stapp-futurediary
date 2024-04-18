d2d7d24f57711b8295a50d7070ba2ceedb2a2f74
Jett Wang <jamiesun.net@gmail.com>
2024-04-18 12:42:32 +0800

    更新未来日记页面配置

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
         let url = new URL(request.url);
-        url.hostname = 'https://futurediary.streamlit.app'
+        url.hostname = 'futurediary.streamlit.app'
         let new_request = new Request(url, request);
         return env.ASSETS.fetch(new_request);
     },
